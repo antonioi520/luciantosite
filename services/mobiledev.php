4a2620fae207b2dcf2c457c33bc0fc44d1c24254
antonio <prodoson@gmail.com>
2020-01-04 21:25:24 -0500

    DATABSE AND MOBILE DEV PAGES

---
+++ services/mobiledev.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Lucianto Web Development</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Facebook Opengraph integration: https://developers.facebook.com/docs/sharing/opengraph -->
+    <meta property="og:title" content="">
+    <meta property="og:image" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <meta property="og:description" content="">
+
+    <!-- Twitter Cards integration: https://dev.twitter.com/cards/  -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="">
+    <meta name="twitter:title" content="">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:image" content="">
+
+    <!-- Place your favicon.ico and apple-touch-icon.png in the template root directory -->
+    <link href="img/favicon.ico" rel="shortcut icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
+
+    <!-- Bootstrap CSS File -->
+    <link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Libraries CSS Files -->
+    <link href="../lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../lib/animate-css/animate.min.css" rel="stylesheet">
+    <!--<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+    <link rel="stylesheet" href="../css/style2.css"> <!-- Resource style -->
+    <script src="../js/modernizr.js"></script> <!-- Modernizr -->
+
+    <!-- Main Stylesheet File -->
+    <link href="../css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+<?php include '../views/nav.php' ?>
+
+<section class="cd-intro" id="hero" style="height:430px;" >
+    <div class="hero-container cd-intro-content mask">
+        <div class="wow fadeIn">
+            <div class="hero-logo" style="margin-top: 80px;">
+                <div style="margin-bottom: -50px;">
+                    <img src="../img/logo2.png" alt="logo image" title="" style="margin-left:50px; height: 200px;">
+                </div>
+                <img class="" src="../img/lucianto.png" alt="logo text" style="margin-left: -5px;">
+            </div>
+
+            <h1 data-content="Mobile Development" style="font-weight: 600; margin-top:30px;"><span>Mobile Development</span></h1>
+        </div>
+    </div>
+</section>
+
+<section id="about" style="padding-top: 40px;">
+    <div class="container about-container wow fadeInUp">
+        <div class="row">
+
+            <div class="col-md-7 about-content">
+                <h1 style="font-weight: bold;font-size: 50px;color:black;">Why have a mobile site?</h1>
+                <div class="section-title-divider" style="width:100%;float:left;margin-bottom: 0px;"></div><br>
+                <p class="about-text">
+                    Having a website or application that works on a majority of mobile devices is a huge priority in this day and age for all businesses. Mobile traffic accounted for approximately 51% of all website traffic in 2019 with approximately 50% of consumers say they won’t return to a website if it doesn’t load properly on their mobile phone. This is a huge consumer base that you could potentially be missing out on.
+                </p>
+                <p class="about-text">
+                    Mobile applications make it easier to connect with your customers and is one of the best marketing tools to develop your business. Mobile applications allow your customers to have all your businesses or product's information right at their fingertips.
+                </p>
+                <p class="about-text">
+                    Due to the accessibility from a large number of platforms, you can start reaching a much broader market and increase your engagement among customers all over the world.
+                </p>
+            </div>
+            <div class="col-lg-5 about-img">
+                <img src="../img/webdevelopment/changelater.jpg" style="height:390px;width:600px;margin-top: 10px;">
+            </div>
+
+        </div>
+
+        <br><br><br>
+
+        <div class="row">
+
+            <div class="col-lg-5 about-img">
+                <img src="../img/webdevelopment/responsive-web-design-caktus%20(1).png" style="height:390px;width:600px;margin-top: 10px;float:left;">
+            </div>
+
+            <div class="col-md-7 about-content">
+                <h1 style="font-weight: bold;font-size: 50px;color:black;">How it's done</h1>
+                <div class="section-title-divider" style="width:100%;float:left;margin-bottom: 0px;"></div><br>
+                <p class="about-text">
+                    In our own released products, we have noticed swings of up to 80% of all traffic coming from consumers viewing from their mobile devices. Without a functional mobile website or application, this is a large percentage of our customers businesses that they could potentially be losing out on.
+                </p>
+                <p class="about-text">
+                    This is why we take the time to meticulously design our pages to scale, look good, and be functional on as many devices as possible in order to give you the largest reach for your business.
+                </p>
+                <p class="about-text">
+                    This is done by first detecting the users screen size and then dynamically changing the layout of the page accordingly, giving the user the most optimal display. Due to the large variety now in mobile phones, companies will overlook less common devices which can result in users not being able to receive a viewable page. We avoid this by diligently testing our pages on every resolution and changing layouts where necessary.
+                </p>
+            </div>
+
+        </div>
+
+        <br><br><br>
+
+
+        <div class="row">
+
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <div style="text-align: center;"><a href="#" class="page-button">Get Started Now</a></div>
+            </div>
+            <div class="col-md-4"></div>
+
+
+        </div>
+
+    </div>
+</section>
+
+<?php include '../views/footer.php'; ?>
+</body>
