5ff948cb6eaf78d0d7d8416d87785b85bd248ad0
Luciano728 <lpm72896@gmail.com>
2020-09-21 21:36:11 -0400

    add to footer and style css

--- views/footer.php
+++ views/footer.php
@@ -5,6 +5,18 @@ Footer
     <div class="container">
         <div class="row">
             <div class="col-md-12">
+                <center>
+                    <div class="d-flex justify-content-center">
+                        <p class="footerLinks"><a href="/luciantosite/index.php" style="text-align: center;font-size: small;">Home</a></p>
+                        <p class="footerLinks"><a href="/luciantosite/portfolio.php" style="text-align: center;font-size: small;">Portfolio</a></p>
+                        <p class="footerLinks"><a href="/luciantosite/portfolio/elninja.php" style="text-align: center;font-size: small;">El Ninja Restaurant</a></p>
+                        <p class="footerLinks"><a href="/luciantosite/portfolio/bomes.php" style="text-align: center;font-size: small;">The Bomes Theatre</a></p>
+                    </div>
+                    <br>
+                    <a href="/index.php"><img src="/luciantosite/img/logo2.png" alt="logo" style="height:100px;width:100px;object-fit: cover;"></a>
+                    <a href="/index.php"><img src="/luciantosite/img/lucianto.png" alt="logo" style="height:100px;width:200px;object-fit: contain;"></a>
+                </center>
+                <br>
                 <div class="copyright">
                     &copy; Copyright 2020 <strong>Lucianto</strong>. All Rights Reserved
                 </div>
