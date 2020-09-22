498eca9a546f99205318b4a053f9852eec2e81f4
Luciano728 <lpm72896@gmail.com>
2020-09-21 20:58:09 -0400

    update footer year

--- views/footer.php
+++ views/footer.php
@@ -6,7 +6,7 @@ Footer
         <div class="row">
             <div class="col-md-12">
                 <div class="copyright">
-                    &copy; Copyright 2019 <strong>Lucianto</strong>. All Rights Reserved
+                    &copy; Copyright 2020 <strong>Lucianto</strong>. All Rights Reserved
                 </div>
                 <div class="credits">
 
